e2cdea43c2a33092fc9b5db72e6d64e815492061
Pascal Fleury <pascal@telefleuries.com>
2026-03-02 23:38:28 +0100

    Making color.c use the number of led channels, not pwm.

--- color.c
+++ color.c
@@ -1,21 +1,10 @@
 #include "color.h"
 #include "numeric.h"
-#include "board.h"
+#include "config.h"
 
-/* Color temperatures for each PWM channel, defined by the board.
+/* Color temperatures for each LED channel, defined in config.
  * Must be in ascending order (warmest to coldest). */
-static const unsigned short colorTemps[NUM_PWM_CHANNELS] = PWM_COLOR_TEMPS;
-
-/* Color temperature mapping using board-defined values.
- *
- * The colorTemp range 0x0000..0xFFFF is divided into (NUM_PWM_CHANNELS - 1) zones.
- * Each zone represents a crossfade between two adjacent PWM channels.
- *
- * Within each zone, we linearly interpolate between the two adjacent channels.
- * The result is then scaled by the dimmer value.
- *
- * All math is 16-bit or 32-bit integer, no floating point.
- */
+static const unsigned short colorTemps[NUM_LED_CHANNELS] = LED_COLOR_TEMPERATURES;
 
 /* Scale a 16-bit value by a 16-bit factor.
  * Returns (a * b) >> 16, i.e. treats both as 0.16 fixed point
@@ -36,26 +25,27 @@ void colorCompute(const DmxState *state, PwmState *pwm)
   unsigned short posInZone;
   unsigned short fade;
 
-  /* Initialize all channels to 0 */
+  /* Initialize all PWM channels to 0.
+   * This ensures unused channels (when NUM_LED_CHANNELS < NUM_PWM_CHANNELS)
+   * are explicitly off. */
   for (i = 0; i < NUM_PWM_CHANNELS; i++) {
     pwm->channel[i] = 0;
   }
 
-#if NUM_PWM_CHANNELS == 1
-  /* Single channel: just apply dimmer */
+#if NUM_LED_CHANNELS == 1
+  /* Single LED channel: just apply dimmer */
   pwm->channel[0] = dimmer;
-  return;
 #else
-  /* Number of zones = NUM_PWM_CHANNELS - 1 */
+  /* Number of zones = NUM_LED_CHANNELS - 1 */
   /* Each zone spans (0x10000 / numZones) of the colorTemp range */
-  zoneSize = 0xFFFF / (NUM_PWM_CHANNELS - 1);
+  zoneSize = 0xFFFF / (NUM_LED_CHANNELS - 1);
 
   /* Determine which zone we're in */
   zone = colorTemp / zoneSize;
 
   /* Clamp to last zone */
-  if (zone >= NUM_PWM_CHANNELS - 1) {
-    zone = NUM_PWM_CHANNELS - 2;
+  if (zone >= NUM_LED_CHANNELS - 1) {
+    zone = NUM_LED_CHANNELS - 2;
   }
 
   /* Position within the zone: 0..zoneSize */
@@ -64,62 +54,10 @@ void colorCompute(const DmxState *state, PwmState *pwm)
   /* Fade: 0 at start of zone, 0xFFFF at end of zone */
   fade = (unsigned short)(((unsigned long)posInZone << 16) / zoneSize);
 
-  /* Crossfade between channel[zone] and channel[zone+1] */
+  /* Crossfade between channel[zone] and channel[zone+1].
+   * Only LED channels 0..NUM_LED_CHANNELS-1 are used;
+   * any remaining PWM channels stay at 0. */
   pwm->channel[zone]     = scale16(0xFFFF - fade, dimmer);
   pwm->channel[zone + 1] = scale16(fade, dimmer);
-#endif
-}
-#include "color.h"
-#include "numeric.h"
-#include "config.h"
-
-/* Color temperatures for each channel, defined in config.
- * Must be in ascending order (warmest to coldest). */
-static const unsigned short colorTemps[NUM_COLOR_CHANNELS] = COLOR_TEMPS;
-
-void colorCompute(const DmxState *dmxState, PwmState *pwmState)
-{
-  unsigned short dimmer = dmxState->dimmer;
-  unsigned short colorTemp = dmxState->colorTemp;
-  unsigned char zone;
-  unsigned short zoneSize;
-  unsigned short posInZone;
-  unsigned long warm, cold;
-  unsigned char i;
-
-  /* Initialize all channels to 0 */
-  for (i = 0; i < NUM_PWM_CHANNELS; i++) {
-    pwm->channel[i] = 0;
-  }
-
-#if NUM_COLOR_CHANNELS == 1
-  /* Single channel: just apply dimmer */
-  pwm->channel[0] = dimmer;
-  return;
-#else
-  /* Number of zones = NUM_COLOR_CHANNELS - 1 */
-  /* Each zone spans (0x10000 / numZones) of the colorTemp range */
-  zoneSize = 0xFFFF / (NUM_COLOR_CHANNELS - 1);
-
-  /* Determine which zone we're in */
-  zone = colorTemp / zoneSize;
-
-  /* Clamp to last zone */
-  if (zone >= NUM_COLOR_CHANNELS - 1) {
-    zone = NUM_COLOR_CHANNELS - 2;
-  }
-
-  /* Position within the zone (0-65535) */
-  posInZone = colorTemp - (zone * zoneSize);
-
-  /* Linear interpolation between warm (zone) and cold (zone+1).
-   * warm = (zoneSize - posInZone) / zoneSize
-   * cold = posInZone / zoneSize
-   * Scale by dimmer and use 32-bit to avoid overflow. */
-  warm = ((unsigned long)(zoneSize - posInZone) * dimmer) / zoneSize;
-  cold = ((unsigned long)posInZone * dimmer) / zoneSize;
-
-  pwm->channel[zone] = (unsigned short)warm;
-  pwm->channel[zone + 1] = (unsigned short)cold;
 #endif
 }
--- color.h
+++ color.h
@@ -8,22 +8,12 @@
  * Maps dimmer and colorTemp to the correct mix of warm/cold
  * white LEDs across the available PWM channels.
  *
+ * Only the first NUM_LED_CHANNELS PWM channels are used for
+ * color mixing. Any remaining PWM channels are set to 0.
+ *
  * state: current DMX state (dimmer + colorTemp)
  * pwm:   output PWM state to be filled in
  */
 void colorCompute(const DmxState *state, PwmState *pwm);
-
-#endif
-#ifndef COLOR_H
-#define COLOR_H
-
-#include "dmx.h"
-#include "pwm.h"
-
-/* Compute PWM channel values from DMX state.
- * Takes the dimmer and color temperature from dmxState,
- * and fills in the pwmState with the appropriate duty cycles
- * for each PWM channel to achieve the desired color temperature. */
-void colorCompute(const DmxState *dmxState, PwmState *pwmState);
 
 #endif
