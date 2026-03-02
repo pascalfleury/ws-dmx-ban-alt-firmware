259ed73aed96359d13ddeef2e964c0fb10807e4c
Pascal Fleury <pascal@telefleuries.com>
2026-03-02 23:34:17 +0100

    renaming.

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
 #define BOARD_MINI_DMX_3CH_V1_2
 //#define BOARD_WS_DMX_3CH_BAN_REV_4_6
 
-/* Number of color temperature channels used.
+/* Number of LED channels used for color temperature mixing.
  * Must not exceed NUM_PWM_CHANNELS defined by the board. */
-#define NUM_COLOR_CHANNELS 3
+#define NUM_LED_CHANNELS 3
 
 /* Color temperatures of the LEDs on each channel (in Kelvin).
  * Must be listed in ascending order (warmest first).
- * Array length must equal NUM_COLOR_CHANNELS. */
-#define COLOR_TEMPS { 2700, 4000, 6500 }
+ * Array length must equal NUM_LED_CHANNELS. */
+#define LED_COLOR_TEMPERATURES { 2700, 4000, 6500 }
